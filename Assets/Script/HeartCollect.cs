4ca15b3060793ffe84c1ea1e81445c31a1992a9d
Florian Bucher <florian.bucher@bbv.ch>
2020-07-11 18:46:07 +0200

    Episode 4

---
+++ Assets/Script/HeartCollect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HeartCollect : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource collectSound;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        this.transform.Rotate(new Vector3(0,1,0), this.rotationSpeed);
+    }
+
+    public void OnTriggerEnter()
+    {
+        this.collectSound?.Play();
+        this.gameObject.SetActive(false);
+    }
+}
